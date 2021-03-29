a1f9b63a20a8be2bba0b27829073e9690ff1ce32
Damir Kuzovatov <kuz.damir.01@gmail.com>
2021-03-29 16:32:53 +0300

    reformatting data and training new model

---
+++ pythonProject/2_words.py
@@ -0,0 +1,52 @@
+import os
+import pickle
+from collections import defaultdict, Counter
+
+import numpy as np
+
+from tensorflow.keras.layers import Dense, Embedding, InputLayer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
+
+import tensorflow as tf
+from tensorflow.keras.layers import LSTM
+from tensorflow.python.keras.layers import Dropout, Bidirectional
+from tensorflow.python.keras.models import load_model
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+with open('textv2.txt', 'r', encoding='utf8') as f:
+    text = f.read()
+
+max_words = 10000
+inp_words = 2
+tokenizer = Tokenizer(num_words=max_words, lower=False, split=' ', char_level=False)
+tokenizer.fit_on_texts([text])
+pickle.dump(tokenizer, open('tokenizer.obj', 'bw'))
+a = defaultdict(list)
+sentences = tokenizer.texts_to_sequences(text.split('\n'))
+x_train = list()
+y_train = list()
+for sentence in sentences:
+    if len(sentence) < inp_words + 1:
+        continue
+    if len(x_train) != len(y_train):
+        print(len(x_train))
+        break
+    for i in range(len(sentence)-inp_words):
+        x_train.append(sentence[i:i+inp_words])
+        y_train.append(sentence[i+inp_words])
+print(len(x_train), len(y_train))
+X = np.array(x_train)
+Y = to_categorical(np.array(y_train))
+print(len(Y))
+model = Sequential()
+model.add(Embedding(max_words, 256))
+model.add(LSTM(128))
+model.add(Dense(max_words, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+print(model.summary())
+history = model.fit(X, Y, batch_size=64, epochs=50)
+model.save('1_word.h5')
---
+++ pythonProject/data_cleaner.py
@@ -0,0 +1,21 @@
+import re
+import unicodedata
+
+filename = 'korolenko.txt'
+with open(filename, 'r') as f:
+    text = f.read()
+text = unicodedata.normalize('NFD', text)
+text = re.sub('[.,!?:;"\'«»—…]|\xa0|\xc2\xa0', '\n', text)
+text = re.sub('\[|]', '', text)
+# text = re.sub(r',| -|- ', ' ', text)
+# text = re.sub(r'[_\[\]]', ' ', text)
+# text = re.sub(r'\d]', '', text)
+# text = re.sub(r'\xa0|\xc2\xa0', '\n', text)
+text = re.sub('\n ', '\n', text)
+# text = re.sub(r'', '\n', text)
+# text = re.sub(r' [гдезлмнпртфхцчшщъыьэю] ', ' ', text)
+# text = re.sub(r'  +', ' ', text)
+text = re.sub('\n\n+', '\n', text)
+
+with open(filename, 'w') as f:
+    f.write(text)
